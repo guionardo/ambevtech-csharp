fac0fe0dff55521ca127e6cdcdcc4672c56bbf0b
Guionardo Furlan <guionardo.furlan@ambevtech.com.br>
2023-02-10 16:06:51 -0300

    Dia-3

--- sources/Dia03/DependencyInjection/WorkerService1/WorkerService1/Worker.cs
+++ sources/Dia03/DependencyInjection/WorkerService1/WorkerService1/Worker.cs
@@ -4,7 +4,7 @@ namespace WorkerService1
     {
         private readonly ILogger<Worker> _logger;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger,IProcessor processador)
         {
             _logger = logger;
         }
@@ -13,8 +13,9 @@ namespace WorkerService1
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                processador.faz_alguma_coisa();
+                //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                //await Task.Delay(1000, stoppingToken);
             }
         }
     }
