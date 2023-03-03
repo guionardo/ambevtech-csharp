2b328e8895e1a76e07f42eb42ea9de89910f0ca8
Guionardo Furlan <guionardo.furlan@ambevtech.com.br>
2023-03-03 15:37:01 -0300

    Day 5

--- sources/Dia06/CollectionsParseAndLinq/Console/Program.cs
+++ sources/Dia06/CollectionsParseAndLinq/Console/Program.cs
@@ -5,14 +5,24 @@ var downloader = new CachedDataDownloader();
 using var reader = downloader.GetReaderFromUrl(EletricVehiclePopulationData.URL);
 var vehicles = EletricVehiclePopulationCsvParser.ReadFromCsv(reader);
 
-var tesla = vehicles.Where(v => v.Make == "TESLA").ToImmutableArray();
+var teslaCars = vehicles
+    .Where(v => v.Make == "TESLA").ToList();
 
-foreach (var vehicle in tesla)
+var allTelsaCarsRunsMoreThan100Miles = teslaCars
+    .All(v => v.EletricRange > 100);
+
+Console.WriteLine(allTelsaCarsRunsMoreThan100Miles);
+
+var minimumEletricRange = teslaCars.Min(n => n.EletricRange);
+
+Console.WriteLine(minimumEletricRange);
+
+var models = teslaCars
+    .DistinctBy(t => t.Model)
+    .Select(t => t.Model)
+    .ToList();
+
+foreach (var model in models)
 {
-    Console.WriteLine(vehicle);
-    
+    Console.WriteLine(model);
 }
-// foreach (var vehicle in vehicles)
-// {
-//     Console.WriteLine(vehicle);
-// }
